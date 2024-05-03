a0b66d9b32b2423d4105949df872be71c5ffaad8
Jayaram Jai <jayaramvs1243@gmail.com>
2024-05-03 11:14:23 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Apache Spark project analyzing the IPL data',
+    author='Jayaram Jai',
+    license='',
+)
